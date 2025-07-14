d6a0e08346b19d83fc187692a9ce9bd8a2dde626
Jesse Wilson <jwilson@squareup.com>
2025-07-14 19:36:24 -0400

    Fix Kotlin/Native zstd builds

--- okio-zstd/build.gradle.kts
+++ okio-zstd/build.gradle.kts
@@ -113,6 +113,7 @@ cklib {
     compilerArgs.addAll(
       listOf(
         //"-DDUMP_LEAKS=1", // For local testing ONLY!
+        "-DZSTD_DISABLE_ASM=1", // We haven't hooked up X86 assembly in Kotlin/Native builds.
         "-DKONAN_MI_MALLOC=1",
         "-Wno-unknown-pragmas",
         "-ftls-model=initial-exec",
